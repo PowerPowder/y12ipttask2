07385fcbd8a088654fb63c6b38bbad9073492448
Reagan <reaganbrasch@gmail.com>
2019-04-27 12:39:00 +1000

    Website with landing page

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Laptop Business</title>
+   <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+   <div id="wrapper">
+      <div class="navbar-outer">
+         <div class="navbar-inner">
+            <div class="navbar-logo">
+               <a class="logo" href="#"><img src="images/logo.png"></a>
+               <h1 class="company-name">Laptops</h1>
+            </div>
+            <div class="navbar-links">
+               <ul class="links">
+                  <li><a href="home">HOME</a></li>
+                  <li><a href="products">PRODUCTS</a></li>
+                  <li><a href="services">SERVICES</a></li>
+                  <li><a href="contact">CONTACT</a></li>
+                  <li><a href="about">ABOUT</a></li>
+                  <li class="sign-in"><a href="login">SIGN-IN</a></li>
+               </ul>
+            </div>
+         </div>
+      </div>
+
+      <div class="landing">
+         <img class="landing-image" src="images/temp-background.jpg">
+         <div class="landing-images section">
+            <img class="slider-image" src="images/temp-background.jpg">
+            <img class="slider-image" src="images/temp-background.jpg">
+            <img class="slider-image" src="images/temp-background.jpg">
+            <img class="slider-image" src="images/temp-background.jpg">
+            <img class="slider-image" src="images/temp-background.jpg">
+         </div>
+      </div>
+      <div class="section products">
+         Best Selling products
+      </div>
+      <div class="section services">
+         What we also do
+      </div>
+      <div class="section about">
+         Who are we
+      </div>
+      <div class="section contact">
+         How you can contact us
+      </div>
+   </div>
+</body>
+</html>
