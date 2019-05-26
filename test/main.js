26e3133e4701a347e9566f176c4bd6f19598ffa8
Reagan <reaganbrasch@gmail.com>
2019-05-26 23:06:32 +1000

    made sign-out on navbar remove cookie of user and login_process creates cookie user with session username

---
+++ test/main.js
@@ -0,0 +1,76 @@
+function advanced_search() {
+   if (document.getElementById('advanced-search').checked) {
+      let thing =
+         'display:grid\n' +
+         'grid-template-columns: 2fr 1fr 2fr 1fr;\n' +
+         'grid-gap: 0 0.05em;\n' + 
+         'padding-top: 0.05em;\n';
+
+      console.log(thing);
+   } else {
+      alert("checkn't");
+   }
+}
+
+function showProducts(str) {
+   if (str=="") {
+      document.getElementById("product-wrapper").innerHTML="";
+      return;
+   } 
+   if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+   } else { // code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+   }
+   xmlhttp.onreadystatechange=function() {
+      if (this.readyState==4 && this.status==200) {
+         document.getElementById("product-wrapper").innerHTML=this.responseText;
+      }
+   }
+   xmlhttp.open("GET","show_products.php?query="+str,true);
+   xmlhttp.send();
+}
+
+function setCookie(name, value) {
+   document.cookie = name + "=" + value;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie(id) {
+   var cart=getCookie("cart");
+   if (cart === "") {
+      setCookie('cart', id + ' ');
+   } else {
+      setCookie('cart', getCookie('cart') + ' ' + id);
+   }
+}
+
+function items() {
+   var products = getCookie('cart');
+   products = products.split(' ').map(Number);
+
+   var cart_items = document.getElementById('cart-items');
+   if (getCookie('cart') === "") {
+      cart_items.innerHTML = 0;
+   } else {
+      cart_items.innerHTML = products.length;
+   }
+}
+
+items();
---
+++ test/order.php
@@ -0,0 +1,87 @@
+<?php
+   $duplicates = array_count_values(explode(' ', $_COOKIE['cart']));
+   $cart = explode(' ', $_COOKIE['cart']);
+   $num_of_products = count($cart);
+
+   $total_cost = 0;
+
+   include 'config.php';
+
+   if ($cart[0] != "") {
+      foreach ($cart as &$value) {
+         $sql = 'SELECT * FROM products WHERE id = ' . $value;
+         $result = mysqli_query($conn, $sql);
+         $resultCheck = mysqli_num_rows($result);
+
+         if ($resultCheck > 0) {
+            while ($data = mysqli_fetch_assoc($result)) {
+               $total_cost += $data['price'];
+            }
+         }
+      }
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Receipt</title>
+   <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+      }
+
+      td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 0.25em;
+      }
+   </style> 
+</head>
+<body>
+   <a href="index.php">Home</a> 
+   <h1>Reciept for: </h1> 
+   <h4>Products:</h4>
+   <table>
+      <tr>
+         <th>Amount</th> 
+         <th>Product id</th>
+         <th>Name</th>
+         <th>Price</th>
+      </tr>
+
+      <?php
+      $cart = array_unique(explode(' ', $_COOKIE['cart']));
+      $num_of_products = count($cart);
+
+      foreach ($cart as &$value) {
+         if ($value == "") {
+            $num_of_products = 0;
+         }
+
+         if ($cart[0] != "") {
+            $sql = 'SELECT * FROM products WHERE id = ' . $value;
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+
+            if ($resultCheck > 0) {
+               while ($data = mysqli_fetch_assoc($result)) {
+                  print '<tr><td>'.$duplicates[$value].'</td><td>'.$data['id'].'</td><td>'.$data['name'].'</td><td style="text-align:right;">'.$data['price'].'</td></tr>';
+               }
+            }
+         }
+      }
+   ?>
+
+   <tr>
+      <td></td>
+      <td></td>
+      <td style="text-align: right;"><b>Total:</b></td>
+      <td><?php echo '<b>$'.$total_cost.'<b>' ?></td>
+   </tr> 
+   </table>
+</body>
+</html>
